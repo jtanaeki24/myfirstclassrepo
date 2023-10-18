60977f0bb80506db37c8f916d2eaff6b4bbb013a
Jerome T <jtanaeki@yahoo.com>
2023-10-18 18:47:33 -0400

    changed path of web app url and changed back mapping of application

--- src/main/java/com/springboot/app/WelcomeController.java
+++ src/main/java/com/springboot/app/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-	@GetMapping("/")
+	@GetMapping("/welcome")
 	public String welcome() {
 		return "welcome to spring boot app development";
 	}
