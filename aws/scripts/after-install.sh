b331c905a2e69d3ec178e24452eac44564f49e55
Jerome T <jtanaeki@yahoo.com>
2023-10-19 20:44:12 -0400

    updated s3 bucket name

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://	codedeploystack-webappdeploymentbucket-p0xxum29tq35/springboot-first-app-0.0.1-SNAPSHOT.jar /usr/local/tomcat9/webapps/springboot-first-app-0.0.1-SNAPSHOT.jar
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-16z7xtprj7sz6/springboot-first-app-0.0.1-SNAPSHOT.jar /usr/local/tomcat9/webapps/springboot-first-app-0.0.1-SNAPSHOT.jar
 
 
 # Ensure the ownership permissions are correct.
